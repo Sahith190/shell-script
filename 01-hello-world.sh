fc2820c1d914d1e3f30618d96ee405e2d4de8942
Sahith posa <sahith.posa1997@gmail.com>
2023-08-21 16:18:50 +0530

    attach document

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello world execute linux"
+
+echo "Ramesh: Hi Good morning Suresh"
+echo "Suresh: Good morning Ramesh"
+echo "Ramehs:How are you Suresh"
+echo "Suresh:Fine Ramesh"
