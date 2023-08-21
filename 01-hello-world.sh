64b833d132565134d3ccf33d2293d0de2ff2195e
Sahith posa <sahith.posa1997@gmail.com>
2023-08-21 16:24:00 +0530

    attach document

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+PERSON1=Ramesh
+PERSON2=Suresh
 echo "Hello world execute linux"
 
-echo "Ramesh: Hi Good morning Suresh"
-echo "Suresh: Good morning Ramesh"
-echo "Ramehs:How are you Suresh"
-echo "Suresh:Fine Ramesh"
+echo "$PERSON1: Hi Good morning $PERSON2"
+echo "$PERSON2: Good morning $PERSON1"
+echo "$PERSON1:How are you $PERSON2"
+echo "$PERSON2:Fine $PERSON1"
