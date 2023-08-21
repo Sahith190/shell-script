bfaadaa2dedfcc944f29da1879e88ca545d368cb
Sahith posa <sahith.posa1997@gmail.com>
2023-08-21 16:34:54 +0530

    attach document

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "Hello world execute linux"
+
+echo "$PERSON1: Hi Good morning $PERSON2"
+echo "$PERSON2: Good morning $PERSON1"
+echo "$PERSON1:How are you $PERSON2"
+echo "$PERSON2:Fine $PERSON1"
