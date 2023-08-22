2e5c45979c5a9b4e5217004531651483f1934ecf
Sahith posa <sahith.posa1997@gmail.com>
2023-08-22 09:09:57 +0530

    mysql

---
+++ 08-install-mysql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+   echo "plse run this script"
+exit 1
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+   echo "Installation of mysql is error"
+   exit 1
+else
+   echo "Instalation of mysql is success"
+fi
