1fb14f4784dfb23d0a2042e84ae09a9a1fd73da4
Sahith posa <sahith.posa1997@gmail.com>
2023-08-22 09:31:03 +0530

    mysql

---
+++ 09-functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+#functions using
+VALIDATE(){
+   if [ $1 -ne 0 ]
+then
+   echo "Installation... FAILURE"
+   exit 1
+else
+   echo "Instalation... SUCCESS"
+fi
+}
+
+if [ $USERID -ne 0 ]
+then
+   echo "plse run this script"
+exit 1
+fi
+
+yum install mysql -y
+
+VALIDATE $? " INSTALLATION MYSQL SUCCESS"
+
+
+yum install postfix -y
+
+VALIDATE $? " INSTALLATION POSTFIX SUCCESS"
