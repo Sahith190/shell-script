0254bbda11ad4b5daefba4e2ef011d41b2f8dd44
Sahith posa <sahith.posa1997@gmail.com>
2023-08-23 11:40:18 +0530

    delete log files

---
+++ 12-delete-old-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+APP_LOGS_DIR=/home/centos/app-logs
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGSDIR=/home/centos/shellscript-logs
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
+FILES-TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+echo "$FILES_TO_DELETE"
