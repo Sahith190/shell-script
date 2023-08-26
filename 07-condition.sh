6e09e92f8f5a0cdc50f7e4995447a4490d458d70
Sahith posa <sahith.posa1997@gmail.com>
2023-08-26 16:53:47 +0530

    added shipping

--- 07-condition.sh
+++ 07-condition.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-NUMBER=$1
+# NUMBER=$1
 
-if [ $NUMBER -gt 10 ];
+# if [ $NUMBER -gt 10 ];
+# then
+#    echo "$NUMBER is greater than 10"
+# else
+#    echo "$NUMBER is not greater than 10"
+# fi
+
+
+NAME=$roboshop
+
+if [ $NAME -ne 0 ]
 then
-   echo "$NUMBER is greater than 10"
+   echo "if name is not there it will create"
 else
-   echo "$NUMBER is not greater than 10"
+   echo "if name already exists"
 fi
